68d14e9c1764a92b14beb755adb986ac21067a3d
oded-ivry <odeded222@gmail.com>
2020-07-27 19:18:59 +0300

    connection test

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -1,3 +1,7 @@
 export async function getProducts(num) {
-  return await (await fetch(`/products/all-products?page=${num}`)).json();
+  return await (
+    await fetch(
+      `https://arcane-citadel-42275.herokuapp.com/products/all-products?page=${num}`
+    )
+  ).json();
 }
